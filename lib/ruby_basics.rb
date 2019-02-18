bb5e9645bc8092bb3ca147bfc111713207561091
stupendous-trainer-5541 <peter.c.viss@biola.edu>
2019-02-18 05:01:08 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,33 @@
 def division(num1, num2)
- 
+ num1 / num2
 end
 
 def assign_variable(value)
-  
+return "#{value}"
 end
 
-def argue
-  
-end
-
-def greeting
+def argue(phrase)
+  return "#{phrase}"
 
 end
 
-def return_a_value
+def greeting(greeting, name)
+
 
 end
 
-def last_evaluated_value
+def return_a_value(phrase = "Nice")
+  "#{phrase}"
+return "#{phrase}"
+end
+
+def last_evaluated_value(phrase = "expert")
+  return "#{phrase}"
+  "#{phrase}"
 
 end
 
-def pizza_party
+def pizza_party(pizza = "cheese")
+return "#{pizza}"
 
 end
